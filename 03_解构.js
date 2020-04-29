337d84e93fe39fcabe3d0d7eb9d808e091867f59
高境杉 <1359287622@qq.com>
2020-04-29 19:21:23 +0800

    解构

---
+++ 03_解构.js
@@ -0,0 +1,17 @@
+/* var obj1 = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+var obj2 = {
+  ...obj1,
+  b: 10,
+};
+console.log(obj1);
+console.log(obj2); */
+
+// *****************************************
+var arr1 = [1, 2, 3, 4];
+var arr2 = [...arr1, 5];
+console.log(arr1);
+console.log(arr2);
